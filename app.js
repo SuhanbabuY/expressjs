6c0e6d267d9529ba362e44eb1588ad3774b5790b
Suhanbabu <Suhanbabu@gmail.com>
2026-07-19 18:30:41 +0530

    17-07-2026

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const layout = require('express-ejs-layouts');
+const db = require('./config/db.js');
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.use(layout);
 app.set('layout', 'layouts/main');
 app.get('/', (req, res) => {
-    res.render('index', { title: 'home', message: 'Student Management System' });
+    res.render('index', { title: 'home', message: 'Hello there!' });
 });
+app.get('/connect', (req, res) => {
+
+    db.connect((err) => {
+        if (err) {
+            res.send('Error connecting to the database!');
+        } else {
+            db.query('SELECT * FROM students limit 2', (err, results) => {
+                if (err) {
+                    res.send('Error executing query!');
+                } else {
+                    console.log(results);
+                    res.send(results);
+                }
+            });
+        }
+    });
+});
+
 app.get('/student', (req, res) => {
-    res.render('student/index', {  title: 'Student Page', message: 'You are ! ' });
+    db.connect((err) => {
+        if (err) {
+            res.send('Error connecting to the database!');
+        } else {
+            db.query('SELECT * FROM students', (err, results) => {
+                if (err) {
+                    res.send('Error executing query!');
+                } else {
+                    console.log(results);
+                    res.render('student/index', { title: 'Student Page', message: 'Welcome to the student page!', students: results });
+                }
+            });
+        }
+    });
+
 });
 app.get('/student/:id/show', (req, res) => {
     const studentId = req.params.id;
