9ff91a5eae76a3e3ca4541237dd502f4ffecfacf
Suhanbabu <Suhanbabu@gmail.com>
2026-07-29 17:10:11 +0530

    28-07-2026 delete operation

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const layout = require('express-ejs-layouts');
 const db = require('./config/db.js');
 const app = express();
+const methodOverride = require('method-override');
+
 const port = 3000;
 app.set('view engine', 'ejs');
 app.use(layout);
 app.set('layout', 'layouts/main');
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.render('index', { title: 'home', message: 'Hello there!' });
@@ -24,6 +27,25 @@ app.get('/connect', (req, res) => {
                 } else {
                     console.log(results);
                     res.send(results);
+                }
+            });
+        }
+    });
+});
+
+//student delete route
+app.delete('/student/:id', (req, res) => {
+    const studentId = req.params.id;
+    console.log(`Deleting student with ID: ${studentId}`);
+    db.connect((err) => {
+        if (err) {
+            res.send('Error connecting to the database!');
+        } else {
+            db.query('DELETE FROM students WHERE id = ?', [studentId], (err, results) => {
+                if (err) {
+                    res.send('Error executing query!' + err);
+                } else {
+                    res.redirect('/student');
                 }
             });
         }
